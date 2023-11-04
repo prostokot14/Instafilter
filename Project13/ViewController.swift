7346eb16902e88dd9e6e43bf2222a06d9a8c650b
Anton Kashnikov <anton.kashnikoff@icloud.com>
2023-11-04 22:30:17 +0100

    Added methods for importing picture

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -8,13 +8,37 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    // MARK: - IBOutlets
+    
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UILabel!
     
+    // MARK: - Private properties
+    
+    private var currentImage: UIImage!
+    
+    // MARK: - UIViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add, target: self, action: #selector(importPicture)
+        )
     }
+    
+    // MARK: - Private methods
+    
+    @objc
+    private func importPicture() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.allowsEditing = true
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true)
+    }
+    
+    // MARK: - IBActions
 
     @IBAction func changeFilter(_ sender: Any) {
     }
@@ -23,5 +47,20 @@ final class ViewController: UIViewController {
     }
     
     @IBAction func intensityChanged(_ sender: Any) {
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate, UINavigationControllerDelegate
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
+    ) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        dismiss(animated: true)
+        
+        currentImage = image
     }
 }
