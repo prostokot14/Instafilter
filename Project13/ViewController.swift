44a73a5d3287477288b29a8a3cacd090c8affe68
Anton Kashnikov <anton.kashnikoff@icloud.com>
2023-11-04 22:12:17 +0100

    Created outlets and actions for the UI-elements

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
 }
-
